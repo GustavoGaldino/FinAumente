59eb1cfba559a41ad957b1b8c0d1ec03ae45568b
gustavogaldino <gcgm@cin.ufpe.br>
2021-12-12 19:56:26 -0300

    Added add-friend page

--- app/src/App.js
+++ app/src/App.js
@@ -1,14 +1,14 @@
 import { Routes, BrowserRouter as Router, Route } from 'react-router-dom'
 
+import './assets/styles/global.css'
+
 import Homepage from './Pages/Homepage/Homepage'
 import Register from './Pages/Register/Register'
 import Login from './Pages/Login/Login'
 import Journey from './Pages/Journey/Journey'
-
-
-import './assets/styles/global.css'
 import Hamburguer from './Pages/Hamburguer/Hamburguer'
 import Friends from './Pages/Friends/Friends'
+import AddFriend from './Pages/AddFriend/AddFriend'
 
 function App() {
 	return (
@@ -20,6 +20,7 @@ function App() {
 				<Route path="/journey" element={ <Journey /> } />
 				<Route path="/hamburguer" element={ <Hamburguer /> } />
 				<Route path="/friends" element={ <Friends /> } />
+				<Route path="/add-friend" element={ <AddFriend /> } />
 			</Routes>
 		</Router>
 	);
---
+++ app/src/Pages/AddFriend/AddFriend.js
@@ -0,0 +1,38 @@
+import './AddFriend.css'
+
+import { Link } from 'react-router-dom';
+
+import Input from '../Register/Input';
+
+import { RiArrowGoBackFill } from 'react-icons/ri';
+
+const AddFriend = () => {
+    return (
+        <div className="add-friend-container">
+            <Link to="/friends">
+                <RiArrowGoBackFill
+                    size="2rem"
+                    style={{
+                        position: "absolute",
+                        left: "2rem",
+                        top : "2rem",
+                        fontWeight: "bold",
+                        cursor: "pointer",
+                        color: "FFC745"
+                    }}
+                />
+            </Link>
+            <div className="add-friend-input">
+                <input placeholder="E-mail do amigo"/>
+            </div>
+            <div className="add-friend-btn">
+                <Link to="/friends">
+                    <button>Adicionar Amigo</button>
+                </Link>
+            </div>
+        </div>
+    );
+}
+
+
+export default AddFriend;
